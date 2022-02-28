23665f4df111317f0f67609b67066dbcb5a98e44
Beatrice <beatricebass94@gmail.com>
2022-02-28 16:10:06 -0500

    checkForWin, resetBoard

--- script.js
+++ script.js
@@ -2,37 +2,50 @@ const gameboard = ( () => {
   let board = Array(9).fill(null); 
 
   const resetBoard = () => {
-    gameboard.board = new Array(9).fill(null);
+    board = Array(9).fill(null);;
+    getBoard();
   };
   
-  // gets the current board array and applies the contents to the playing board 
+  // gets the current board array and applies the contents to the game board 
   const getBoard = () => {
     let fields = Array.from(document.querySelectorAll(".field"));
-
-    for (i = 0; i < board.length; i++) {
+    for (i = 0; i < (board).length; i++) {
       fields[i].textContent = board[i];
       }
-      console.log(board);
   };
 
   //get value from DOM (marker and position), set board array value
   const setCell = (marker, fieldPos) => {
     board[fieldPos] = marker;
-    console.log(board);
   };
 
-  const checkForWin = () => {
-    const winConditions = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  const checkForWin = (turnCounter, marker) => {
+    if (turnCounter === 9) {
+      const tieMsg = document.createElement("p");
+      tieMsg.textContent = "Tie Game!";
+      const resetBtn = document.createElement("button");
+      resetBtn.innerText = "RESET"
+      resetBtn.addEventListener("click", resetBoard);
+      document.querySelector(".gameControls").append(tieMsg, resetBtn);
+    }
+    else {
+      winConditions.forEach((condition) => {
+        if(board[condition[0]]=== marker && board[condition[1]] === marker && board[condition[2]] === marker)
+        console.log("winner!");
+      })
   }
+}
+
 
   return {setCell, resetBoard, getBoard, setCell, checkForWin};
   
@@ -42,42 +55,53 @@ const gameboard = ( () => {
 
 // player needs to get marker assigned, depending which player plays, the marker is different..and that in turn will change the field..which should update the board array
 const PlayerFactory = (name, marker) => {
-    marker;
     name;
-    return(name, marker)
+    marker;
+    return {name, marker}
 };
 
-player1Name = "Will";
-player2Name = "Jen";
+let player1Name = "Will";
+let player2Name = "Jen";
 
 const Gameplay = ( () => {
+
   let player1 = PlayerFactory(player1Name, "x");
   let player2 = PlayerFactory(player2Name, "o");
+  let turnCounter = 0
+
   let currentPlayer = player1;
-  
-  let fields = Array.from(document.querySelectorAll(".field"));
 
-    for (let i = 0; i < fields.length; i++) {
-      let fieldPos = fields[i].getAttribute("id");
-      fields[i].addEventListener("click", () => {
-        marker = currentPlayer;
-        gameboard.setCell(marker, fieldPos);
-        gameboard.getBoard();
-        switchPlayers();
-      })
+  const makeMove = (field) => {
+    if (field.textContent != "") {
+      return;
     }
-  
-
-  const makeMove = () => {
-    // let marker = currentPlayer
-    let fields = Array.from(document.querySelectorAll(".field"));
-
-    fields.addEventListener("click", () =>{
-      for (field in fields)
-      field.value = marker;
-      setCell(marker, field)
-    })
+    let fieldPos = field.getAttribute("id");
+    marker = currentPlayer.marker;
+    turnCounter += 1;
+    gameboard.setCell(marker, fieldPos);
+    gameboard.getBoard();
+    switchPlayers();
+    gameboard.checkForWin(turnCounter, marker);
   }
+
+
+  let fields = Array.from(document.querySelectorAll(".field"));
+  fields.forEach(field => {
+    field.addEventListener("click", () => {
+      makeMove(field)
+    })
+  });
+
+
+  //Stick this in win conditions after
+ 
+
+
+    
+   
+
+
+
 
   const switchPlayers = () => {
     if(currentPlayer===player1){
@@ -85,18 +109,20 @@ const Gameplay = ( () => {
     } else {
       currentPlayer=player1;
     }
-    console.log(currentPlayer);
   }
 
+function start() {
+  const startBtn = document.querySelector(".startBtn");
+  startBtn.addEventListener("click", () => {
+    player1Name = document.querySelector(".player1Name").value;
+    player2Name = document.querySelector(".player2Name").value;
+    console.log(player1Name, player2Name);
+    setPlayers(player1Name. player2Name);
+  });
+}
 
+start();
 
-  return {switchPlayers, makeMove}
-})();
+  return {switchPlayers, start, makeMove, fields, turnCounter}
+  })();
 
-// player1Name = document.querySelector(".player1Name").value;
-// player2Name = document.querySelector(".player2Name").value; 
-// console.log(player1Name, player2Name);
-// const startBtn = document.querySelector(".startBtn");
-//   startBtn.addEventListener("click", () => {
-//     Gameplay(player1Name, player2Name)
-//   });
